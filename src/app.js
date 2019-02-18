8d4fbb96e6f3243c070e5a36ea0f4171add40016
chetin4ever <hetan009@gmail.com>
2019-02-18 22:11:20 +0530

    added react-router-dom to project and using it

--- src/app.js
+++ src/app.js
@@ -2,7 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/styles.scss'; 
 import 'normalize.css/normalize.css';
+import {BrowserRouter,Route} from 'react-router-dom';
+
+const ExpenseDashBoardPage = () =>(
+    <div> this is epense ExpenseDashBoardPage</div>
+)
+const AddExpensePage=()=>(
+    <div> this is expence add page</div>
+)
+const EditExpense=()=>(
+    <div> this is edit expence page</div>
+)
+const Help=()=>(
+    <div>
+        this is help page
+    </div>
+)
+const routes=(
+    <BrowserRouter>
+        <div>
+            <Route path="/" component={ExpenseDashBoardPage} exact={true}/>
+            <Route path="/create" component={AddExpensePage}/>
+            <Route path="/editExpense" component={EditExpense}/>
+            <Route path="/help" component={Help}/>
+        </div>
+        
+    </BrowserRouter>
+);
 
   
-ReactDOM.render(<p>this is my boilerplate app</p>, document.getElementById("app"));
+ReactDOM.render(routes, document.getElementById("app"));
   
