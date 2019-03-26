1e420e1ebe0223163c3565f22223dcb1ccda3a70
chetin4ever <hetan009@gmail.com>
2019-03-26 21:58:33 +0530

    filter data using input control

--- src/components/ExpenseDashBoardPage.js
+++ src/components/ExpenseDashBoardPage.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ExpenseList from './ExpenseList';
+import ExpenseListFilters from './ExpenseListFilters'
 
-
-const ExpenseDashBoardPage = () =>(
-    <div> <ExpenseList/></div>
+const ExpenseDashBoardPage = (props) =>(
+    <div> 
+        <ExpenseListFilters />
+        <ExpenseList/>
+    </div>
 )
 export default ExpenseDashBoardPage;   
